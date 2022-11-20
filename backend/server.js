30973537590b48e9e93f3d62c8dc5ae73bf23e32
Rohan <rohankingkumar8@gmail.com>
2022-11-20 18:53:54 +0530

    DEPLOYMENT Script on backend done

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
-
 const connectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
+
 const app = express();
 dotenv.config();
 connectDB();
@@ -13,21 +14,25 @@ connectDB();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
-// app.get("/api/notes", (req, res) => {
-//   res.json(notes);
-// });
-
-// app.get("/api/notes/:id/", (req, res) => {
-//   const note = notes.find((n) => n._id === req.params.id);
-//   res.send(note);
-// });
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// --------------------------deployment------------------------------
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+//  --------------------------deployment------------------------------
 
 const PORT = process.env.PORT || 5000;
 
